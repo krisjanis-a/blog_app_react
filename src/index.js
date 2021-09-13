f3ee71f8597e8e85670a7bdd891708ec8b0b4328
krisjanis-a <krishjanis96@gmail.com>
2021-09-13 03:01:00 +0300

    build with dev/prod environment baseURL

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ContextProvider } from "./context/Context";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+
+console.log(axios.defaults.baseURL);
+console.log(process.env.NODE_ENV);
 
 ReactDOM.render(
   <React.StrictMode>
